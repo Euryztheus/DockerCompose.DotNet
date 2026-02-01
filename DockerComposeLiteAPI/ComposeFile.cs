f2afcb362b5d38f0aa39a0a9ee1200c9a9a6167f
Nicolas Weippl <nicolas.weippl@proton.me>
2026-02-01 23:31:17 +0100

    deserialize yaml to json

---
+++ DockerComposeLiteAPI/ComposeFile.cs
@@ -0,0 +1,9 @@
+namespace DockerComposeLiteAPI;
+
+public class ComposeFile
+{
+  public string version;
+  public string services;
+  public string volumes;
+  public string networks;
+}
--- DockerComposeLiteAPI/ComposeLiteAPI.cs
+++ DockerComposeLiteAPI/ComposeLiteAPI.cs
@@ -1,4 +1,5 @@
-﻿using YamlDotNet.RepresentationModel;
+﻿using Newtonsoft.Json;
+using YamlDotNet.Serialization;
 
 namespace DockerComposeLiteAPI;
 
@@ -7,25 +8,29 @@ public class ComposeLiteAPI
   private string composeFile;
   public ComposeLiteAPI(string composeFile)
   {
-    Console.WriteLine("working");
     this.composeFile = composeFile;
   }
 
-  public void parseComposeFile()
+  public void ParseComposeFile()
   {
 
     var input = new StringReader(composeFile);
-    var yaml = new YamlStream();
-    yaml.Load(input);
+    var deserializer = new DeserializerBuilder().Build();
+    var yamlObject = deserializer.Deserialize(input);
 
-    // Examine the stream
-    var mapping =
-        (YamlMappingNode)yaml.Documents[0].RootNode;
+    var serializer = new SerializerBuilder()
+        .JsonCompatible()
+        .Build();
 
-    foreach (var entry in mapping.Children)
+    var json = serializer.Serialize(yamlObject);
+
+    //Console.WriteLine(json);
+    Dictionary<string, object> cpFile = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+    foreach (var item in cpFile)
     {
-      Console.WriteLine(entry);
+      Console.WriteLine(item.Value);
     }
+
   }
 
 }
